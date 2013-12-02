89805366d73eed4e9b2d62a4436777ed4c95e440
Gabriel Massana <gabriel.massana@irisvalet.com>
2013-12-02 09:47:52 +0000

    first commit

--- pressAndHoldButton/ViewController.m
+++ pressAndHoldButton/ViewController.m
@@ -10,6 +10,9 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIButton *pressButton;
+@property (nonatomic, strong) NSTimer  *timerDown;
+
 @end
 
 @implementation ViewController
@@ -17,13 +20,38 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    // Idea
+    // http://stackoverflow.com/questions/903114/way-to-make-a-uibutton-continuously-fire-during-a-press-and-hold-situation
+    
+    self.pressButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.pressButton setFrame:CGRectMake(20, 50, 280, 50)];
+    [self.pressButton setBackgroundColor:[UIColor lightGrayColor]];
+    [self.view addSubview:self.pressButton];
+    
+    [self.pressButton addTarget:self action:@selector(down:) forControlEvents:UIControlEventTouchDown];
+    [self.pressButton addTarget:self action:@selector(up:) forControlEvents:UIControlEventTouchUpInside];
+    [self.pressButton addTarget:self action:@selector(up:) forControlEvents:UIControlEventTouchUpOutside];
+}
+
+- (void) down:(id) sender
+{
+    self.timerDown = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(print:) userInfo:Nil repeats:YES];
+}
+
+- (void) up:(id) sender
+{
+    [self.timerDown invalidate];
+}
+
+- (void) print:(id) sender
+{
+    NSLog(@"down");
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
