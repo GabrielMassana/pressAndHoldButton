14662c25d704aff76e64a2f28ade7b7622ea1987
Gabriel Massana <gabriel.massana@irisvalet.com>
2013-12-02 09:29:38 +0000

    Initial Commit

---
+++ pressAndHoldButton/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  pressAndHoldButton
+//
+//  Created by Gabriel Massana on 12/2/13.
+//  Copyright (c) 2013 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ pressAndHoldButtonTests/pressAndHoldButtonTests.m
@@ -0,0 +1,34 @@
+//
+//  pressAndHoldButtonTests.m
+//  pressAndHoldButtonTests
+//
+//  Created by Gabriel Massana on 12/2/13.
+//  Copyright (c) 2013 Gabriel Massana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pressAndHoldButtonTests : XCTestCase
+
+@end
+
+@implementation pressAndHoldButtonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
